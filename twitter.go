e86fbbf7fbadef74757cb45cc66e0ad6dfab7758
Peter Jönsson <peter.joensson@gmail.com>
2013-06-13 00:06:43 +0200

    First step with function calls in Go
    Really horrible to do os.Exit() in a function call so this will need
    to be cleaned up later on. Baby steps, baby steps...

--- twitter.go
+++ twitter.go
@@ -29,22 +29,28 @@ import (
 	"os"
 )
 
-func main() {
-	url := "https://twitter.com"
+func http_request(url string) []byte {
 	response, error := http.Get(url)
 	if error != nil {
-		fmt.Println("Could not connect to server and retrieve data, exiting")
-		fmt.Println(error)
+		fmt.Println("Could not connect to server and retrieve data, exiting.\n%s",
+			error)
 		os.Exit(1)
 	}
 
 	defer response.Body.Close()
 	contents, error := ioutil.ReadAll(response.Body)
 	if error != nil {
-		fmt.Println("Failed to read HTTP response from server, exiting")
-		fmt.Println(error)
+		fmt.Println("Failed to read HTTP response from server, exiting.\n%s",
+			error)
 		os.Exit(1)
 	}
 
+	return contents
+
+}
+
+func main() {
+	url := "https://twitter.com"
+        contents := http_request(url)
 	fmt.Println("%s", string(contents))
 }
