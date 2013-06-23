31b21e4a4e3717000cea2a45834b9b348b80887c
Peter Jönsson <peter.joensson@gmail.com>
2013-06-24 00:15:01 +0200

    WIP: read stuffz on socket and echo back.

--- twitter.go
+++ twitter.go
@@ -3,32 +3,68 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 )
 
 func http_request(url string) []byte {
 	response, error := http.Get(url)
-	if error != nil {
-		fmt.Println("Could not connect to server and retrieve data, exiting.\n%s",
-			error)
-		os.Exit(1)
-	}
+	check_error(error)
 
 	defer response.Body.Close()
 	contents, error := ioutil.ReadAll(response.Body)
-	if error != nil {
-		fmt.Println("Failed to read HTTP response from server, exiting.\n%s",
-			error)
-		os.Exit(1)
-	}
+	check_error(error)
 
 	return contents
-
 }
 
 func main() {
-	url := "https://twitter.com"
-        contents := http_request(url)
-	fmt.Println("%s", string(contents))
+	// 29418 is the default Gerrit Port, here we should send git
+	// events when receving the "stream-events" command.
+
+
+	//url := "https://twitter.com"
+        //contents := http_request(url)
+	//fmt.Println("%s", string(contents))
+
+	service := ":9000"
+	tcpAddr, error := net.ResolveTCPAddr("ipv4", service)
+	check_error(error)
+
+	listener, error := net.ListenTCP("tcp", tcpAddr)
+	check_error(error)
+
+	for {
+		conn, error := listener.Accept()
+		if error != nil {
+			continue
+		}
+
+		go handle_connection(conn)
+	}
+}
+
+func handle_connection(conn net.Conn) {
+	// close connection on exit
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	_, error := conn.Read(buffer)
+	if error != nil {
+		// do something good to clean up?
+	} else {
+		switch {
+		case string(buffer) == "gerrit":
+			conn.Write([]byte("gerrit command"))
+		default:
+			conn.Write(buffer[0:])
+		}
+	}
+}
+
+func check_error(error error) {
+	if error != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error %s", error.Error())
+		os.Exit(1)
+	}
 }
